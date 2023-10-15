eb829d89fa263fe95ca5d62cac9913eed29c473f
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2023-10-15 08:25:51 +0530

    dashboard done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from './ui/button';
 import { Menu } from 'lucide-react';
 import { UserButton } from '@clerk/nextjs';
+import MobileSidebar from './MobileSidebar';
 
 type NavbarProps = {
     
@@ -10,10 +11,7 @@ type NavbarProps = {
 const Navbar:React.FC<NavbarProps> = () => {
     
     return <div className='flex items-center p-4'>
-        <Button variant="ghost" 
-        size="icon" className='md:hidden'>
-            <Menu />
-        </Button>
+        <MobileSidebar />
         <div className="flex w-full justify-end">
             <UserButton afterSignOutUrl='/' />
         </div>
