f6f50a1ae665be240c53dabd93bc873cc92e7f73
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2023-10-21 00:22:59 +0530

    code and conversation done

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -1,47 +1,53 @@
 "use client";
+
 import * as z from "zod";
 import axios from "axios";
-import Heading from "@/components/Heading";
-import {  MessageSquare } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { Code } from "lucide-react";
 import { useForm } from "react-hook-form";
-import { formSchema } from "./constants";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { ChatCompletionRequestMessage } from "openai";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import { ChatCompletionRequestMessage } from "openai";
 import { cn } from "@/lib/utils";
-import Empty from "@/components/Empty";
+import Heading from "@/components/Heading";
 import Loader from "@/components/Loader";
+import Empty from "@/components/Empty";
 import { UserAvatar } from "@/components/UserAvatar";
 import { BotAvatar } from "@/components/BotAvatar";
+import ReactMarkdown from "react-markdown";
 
-export default function ConversationPage() {
+export const formSchema = z.object({
+  prompt: z.string().min(1, {
+    message: "Prompt is required."
+  }),
+});
+
+const CodePage = () => {
   const router = useRouter();
+  //const proModal = useProModal();
   const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      prompt: "",
-    },
+      prompt: ""
+    }
   });
 
   const isLoading = form.formState.isSubmitting;
+  
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const userMessage: ChatCompletionRequestMessage = {
-        role: "user",
-        content: values.prompt,
-      };
+      const userMessage: ChatCompletionRequestMessage = { role: "user", content: values.prompt };
       const newMessages = [...messages, userMessage];
-
-      const response = await axios.post("/api/conversation", {
-        messages: newMessages,
-      });
+      
+      const response = await axios.post('/api/code', { messages: newMessages });
       setMessages((current) => [...current, userMessage, response.data]);
-
+      
       form.reset();
     } catch (error: any) {
     //   if (error?.response?.status === 403) {
@@ -52,23 +58,22 @@ export default function ConversationPage() {
     } finally {
       router.refresh();
     }
-  };
+  }
 
-  return (
+  return ( 
     <div>
       <Heading
-        title="Conversation"
-        description="Our most advanced conversation model."
-        icon={MessageSquare}
-        iconColor="text-violet-500"
-        bgColor="bg-violet-500/10"
+        title="Code Generation"
+        description="Generate code using descriptive text."
+        icon={Code}
+        iconColor="text-green-700"
+        bgColor="bg-green-700/10"
       />
-
       <div className="px-4 lg:px-8">
         <div>
           <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
+            <form 
+              onSubmit={form.handleSubmit(onSubmit)} 
               className="
                 rounded-lg 
                 border 
@@ -89,27 +94,20 @@ export default function ConversationPage() {
                     <FormControl className="m-0 p-0">
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-                        disabled={isLoading}
-                        placeholder="How do I calculate the radius of a circle?"
+                        disabled={isLoading} 
+                        placeholder="Simple toggle button using react hooks." 
                         {...field}
                       />
                     </FormControl>
                   </FormItem>
                 )}
               />
-              <Button
-                className="col-span-12 lg:col-span-2 w-full"
-                type="submit"
-                disabled={isLoading}
-                size="icon"
-              >
+              <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
                 Generate
               </Button>
             </form>
           </Form>
         </div>
-        <div>
-
         <div className="space-y-4 mt-4">
           {isLoading && (
             <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
@@ -129,16 +127,25 @@ export default function ConversationPage() {
                 )}
               >
                 {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm">
-                  {message.content}
-                </p>
+                <ReactMarkdown components={{
+                  pre: ({ node, ...props }) => (
+                    <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                      <pre {...props} />
+                    </div>
+                  ),
+                  code: ({ node, ...props }) => (
+                    <code className="bg-black/10 rounded-lg p-1" {...props} />
+                  )
+                }} className="text-sm overflow-hidden leading-7">
+                  {message.content || ""}
+                </ReactMarkdown>
               </div>
             ))}
           </div>
         </div>
-
-        </div>
       </div>
     </div>
-  );
+   );
 }
+ 
+export default CodePage;
