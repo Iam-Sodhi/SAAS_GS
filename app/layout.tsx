1f8a5960a573ba36a360a5f5a94f1e3d76e5482b
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2023-12-31 17:21:53 +0530

    new id used

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import Sidebar from '@/components/Sidebar'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +20,8 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}</body>
     </html>
     </ClerkProvider>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react"
 
 
 export default function Home() {
-  return (
-    <main >
-    
-    </main>
-  )
-}
+  const router =useRouter();
+  useEffect(() => {
+    // Check if the current URL is the root URL
+    router.push('/dashboard');
+  }, [router]);
+    return (
+      <main >
+      
+      </main>
+    )
+  }
